426f23384f1b135843fba5d3c102ec356c3cd544
SebastianGuevara <sebas.guevara.velez@gmail.com>
2023-08-11 12:31:43 -0500

    diego profe

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <h1>EL PROFE DIEGO</h1>
       <img alt="Imágen del profe diego" src="diego.jpg" style={{width:"350px"}}/>
+      <h1>CAPÍTULO 1: COMO CREAR UNA RAMA EN GITHUB</h1>
+      <img alt="Imágen del profe diego" src="diego2.jpg" style={{width:"350px"}}/>
     </div>
   );
 }
